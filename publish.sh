d1092fcd1e7ba50a889b8c5c3632fdc8f590356b
Bastien <bzg@gnu.org>
2022-04-19 13:46:23 +0200

    Initial commit

---
+++ publish.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+":"; exec emacs --quick --script "$0" -- "$@" # -*- mode: emacs-lisp; lexical-binding: t; -*-
+
+(dolist (org-file (directory-files-recursively default-directory "\\.org$"))
+  (let ((html-file (concat (file-name-directory org-file)
+			   (file-name-base org-file) ".html")))
+    (if (and (file-exists-p html-file)
+             (file-newer-than-file-p html-file org-file))
+	(message " [skipping] unchanged %s" org-file)
+      (message "[exporting] %s" (file-relative-name org-file default-directory))
+      (with-current-buffer (find-file org-file)
+	(condition-case err
+            (org-html-export-to-html)
+          (error (message (error-message-string err))))))))
