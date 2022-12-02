8f189294c54c031a03dbef7f02d6ca48ee0ddfc9
Bastien <bzg@gnu.org>
2022-12-02 19:22:52 +0100

    publish.sh: Let meetups.org be exported as .ics

--- publish.sh
+++ publish.sh
@@ -2,6 +2,10 @@
 
 ":"; exec emacs --quick --script "$0" -- "$@" # -*- mode: emacs-lisp; lexical-binding: t; -*-
 
+(with-temp-buffer
+  (find-file "meetups.org")
+  (org-icalendar-export-to-ics))
+
 (dolist (org-file (directory-files-recursively default-directory "\\.org$"))
   (let ((html-file (concat (file-name-directory org-file)
 			   (file-name-base org-file) ".html")))
